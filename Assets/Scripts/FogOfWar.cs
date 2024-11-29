4c942fb0cf45ed972de26b5ad139393241cbdc59
Erdi Yunus GÜL <erdiyunusfb@gmail.com>
2024-11-29 22:06:37 +0300

    Uptade

---
+++ Assets/Scripts/FogOfWar.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FogOfWar : MonoBehaviour
+{
+    public Material fogMaterial; // Shader'a sahip materyal
+    public Transform player; // Karakter
+    public float revealRadius = 5.0f; // Açýða çýkan alanýn yarýçapý
+
+    void Update()
+    {
+        // Karakterin pozisyonunu Shader'a gönder
+        fogMaterial.SetVector("_PlayerPosition", new Vector4(player.position.x, player.position.y, player.position.z, 1));
+        fogMaterial.SetFloat("_RevealRadius", revealRadius);
+    }
+}
